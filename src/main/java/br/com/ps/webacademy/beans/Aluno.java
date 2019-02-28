30d34086573dbaf301d9f97724d94c7623bbe124
Diego Santos <diegosan.dev@gmail.com>
2019-02-28 12:01:43 -0300

    bean aluno

---
+++ src/main/java/br/com/ps/webacademy/beans/Aluno.java
@@ -0,0 +1,57 @@
+package br.com.ps.webacademy.beans;
+
+import java.time.LocalDate;
+
+@SuppressWarnings("serial")
+public class Aluno extends BeanBase {
+
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String email;
+	private LocalDate dataNascimento;
+
+	public Aluno() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
