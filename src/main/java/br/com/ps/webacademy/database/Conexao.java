941b2f4c41b01861f27bc171bdd8530170973143
Diego Santos <diegosan.dev@gmail.com>
2019-02-28 12:45:48 -0300

    classe Conexão DB

---
+++ src/main/java/br/com/ps/webacademy/database/Conexao.java
@@ -0,0 +1,26 @@
+package br.com.ps.webacademy.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+	private static Connection connection = null;
+
+	public Conexao() {
+	}
+
+	public static synchronized Connection conexao() {
+		if (connection == null) {
+			try {
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ps_web_academy", "postgres",
+						"postgres");
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+}
